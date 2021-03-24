4ae0d23dabe2b2965fa375002fb0ab80433fc063
Yashwant Rautela <yashwant.rautela@intelligaia.com>
2021-03-24 12:59:56 +0530

    login page in progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./styles.css";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch"
+    }
+  },
+  container: {
+    backgroundColor: "#fff",
+    position: "absolute",
+    height: "98vh",
+    borderRadius: 10
+  }
+}));
 
 export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className={classes.container}>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="standard-basic" label="Standard" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </form>
     </div>
   );
 }
