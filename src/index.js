98d0f9ce6c8238a3c056a3b343e9a18d6d43576a
Yashwant Rautela <yashwant.rautela@intelligaia.com>
2021-03-27 09:53:00 +0530

    Home Page added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import Home from "./Home";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Home />
   </StrictMode>,
   rootElement
 );
